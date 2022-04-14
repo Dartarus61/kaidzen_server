4200b31fcc2c25d23c95220bb6c4c9ee52ca203f
Dartar <07kirgame@gmail.com>
2022-04-14 23:54:40 +0300

    fix

--- backend/config.js
+++ backend/config.js
@@ -6,3 +6,7 @@ dotenv.config({ silent: (process.env.SMTP_USER = "07kirgame@gmail.com") });
 dotenv.config({ silent: (process.env.SMTP_PASS = "191kalinina191_190") });
 dotenv.config({ silent: (process.env.API_URL = "http://localhost:3001") });
 dotenv.config({ silent: (process.env.CLIENT_URL = "http://ya.ru/") });
+dotenv.config({
+  silent: (process.env.DB_HOST =
+    "postgres://pwfltltmagljbh:7a6d15a895c0c0d40c3bc02ee73077dc1043e9d7bd6a1ebd7b61b2864f8799ad@ec2-34-192-210-139.compute-1.amazonaws.com:5432/df7c675ja6oaqm"),
+});
